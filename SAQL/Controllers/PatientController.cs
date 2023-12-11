79ee07f85ac4dede399ec73165264834cf68da43
Ihor Tymoshchuk <ihor.tymoshchuk.pz.2020@lpnu.ua>
2023-12-12 01:15:45 +0200

    test controller added <3

---
+++ SAQL/Controllers/PatientController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAQL.Contexts;
+using SAQL.Entities;
+using Microsoft.EntityFrameworkCore;
+using SAQL.DTO;
+
+namespace SAQL.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PatientController : ControllerBase
+    {
+        private readonly SAQLContext _context;
+
+        public PatientController(SAQLContext context)
+        {
+            _context = context;
+        }
+
+      
+        [HttpGet("GetByDoctorId/{doctorId}")]
+        public async Task<ActionResult<List<PatientDTO>>> GetPatientsByDoctorId(int doctorId)
+        {
+            var patients = await _context.Patients
+            .Where(p => p.DoctorId == doctorId)
+            .Select(p => new PatientDTO
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Surname = p.Surname,
+                DateOfBirth = p.DateOfBirth
+            })
+            .ToListAsync();
+
+            if (patients == null || !patients.Any())
+            {
+                return NotFound(); 
+            }
+
+            return patients;
+        }
+    }
+}
---
+++ SAQL/DTO/PatientDTO.cs
@@ -0,0 +1,14 @@
+﻿using SAQL.Entities;
+
+namespace SAQL.DTO
+{
+    public class PatientDTO
+    {
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+      
+    }
+}
